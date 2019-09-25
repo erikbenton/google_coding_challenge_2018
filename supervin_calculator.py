38f479e67f58c0addb8ad3efbded2b66594f8f25
Erik Benton <EBenton@lab.electrostandards.com>
2019-09-25 16:22:01 -0400

    adding quick check for all non-odd numbers

--- supervin_calculator.py
+++ supervin_calculator.py
@@ -16,8 +16,25 @@
 # The first line of the input gives the number of test cases, T. T test cases follow.
 # Each begins with one line containing an integer N: the integer initially displayed on Supervin's calculator.
 
+
+def quick_check(test_num, is_it_negative, number_digits):
+    # Quick check for already all non-odd numbers
+    quick_check = True
+    # Start index
+    start = 0
+    if is_it_negative:
+        start = 1
+    for j in range(start, start + number_digits):
+        if int(str_displayed[j]) % 2 == 1:
+            quick_check = False
+            break
+    if quick_check:
+        print("Case #{0}: {1}".format(test_num + 1, "0"))
+    return
+
+
 # Number of test cases
-num_cases = input()
+num_cases = int(input())
 
 # Go through each test case...
 for i in range(num_cases):
@@ -28,5 +45,9 @@ for i in range(num_cases):
     # Get the number of digits
     num_digits = len(str_displayed)
     # Check for negative entries
+    is_negative = False
     if str_displayed[0] == '-':
         num_digits -= 1
+        is_negative = True
+    # Quick check for already all non-odd numbers
+    quick_check(i, is_negative, num_digits)
